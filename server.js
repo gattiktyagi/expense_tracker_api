9f7be5e5fd855228573195dc43fd079e29e82e78
Gattik <207578100+gattiktyagi@users.noreply.github.com>
2026-06-05 20:04:56 +0530

    Add authentication middleware and update server configuration

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app=require('./src/app');
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT||3000;
 
 const startServer = ()=>{
     app.listen(PORT, ()=>{
---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = async (req,res,next) => {
+    try{
+        const authHeader=req.headers.authentication;
+
+        if(!authHeader||!authHeader.startsWith('Bearer ')){
+            res.status(401).json({
+                message:"Token missing"
+            });
+        }
+
+        const token = authHeader.split(' ')[1];
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+    }catch(error){
+        return res.status(401).json({
+            message:"Invalid token"
+        });
+    }
+};
+
+module.exports=authMiddleware;
