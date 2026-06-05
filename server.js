259e07f2479fad246af6a4f39007dcdc2c0d5fdf
Gattik <207578100+gattiktyagi@users.noreply.github.com>
2026-06-05 16:27:51 +0530

    Update main entry point to server.js and start script for server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+const app=require('./src/app');
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+const startServer = ()=>{
+    app.listen(PORT, ()=>{
+    console.log(`
+    ┌─────────────────────────────────────┐
+    │     Server Online                   │
+    ├─────────────────────────────────────┤
+    │  Port   : PORT                      │
+    │  URL    : localhost:PORT            │
+    │  Status : \x1b[32mREADY\x1b[0m                     │
+    └─────────────────────────────────────┘
+    `);
+    });
+};
+
+startServer();
