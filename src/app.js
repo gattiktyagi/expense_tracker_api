1289aaba7159c58260e3bfae76ec9c2a931515f5
Gattik <207578100+gattiktyagi@users.noreply.github.com>
2026-06-10 16:16:35 +0530

    Add CORS middleware to enable cross-origin requests

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authRoutes = require("./auth/auth.routes");
 const expenseRoutes = require("./expenses/expenses.routes");
+const cors=require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);
